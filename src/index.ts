2c42240bdf35cd2a5be5c5610c4908d8444bd192
AhmedYassen <ahmed.ibrahim.yassen@gmail.com>
2023-06-13 21:07:46 +0300

    populate dogs table with required data

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,28 @@ const PORT = process.env.SERVER_PORT || 3000;
 
 const start = async () => {
   await connection.sync();
+
+  const dogsRepository = connection.getRepository(DBDog);
+  await dogsRepository.findOrCreate({
+    where: { name: "Neo" },
+    defaults: {
+      name: "Neo",
+      color: "red & amber",
+      tail_length: 22,
+      weight: 32,
+    },
+  });
+
+  await dogsRepository.findOrCreate({
+    where: { name: "Jessy" },
+    defaults: {
+      name: "Jessy",
+      color: "black & white",
+      tail_length: 7,
+      weight: 14,
+    },
+  });
+
   console.log("Connected to db successfully");
   app.listen(() => {
     console.log(`Server is listening on port ${PORT}`);
