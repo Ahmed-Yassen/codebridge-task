39097e205c90bf2be9db5c3ff25f73f363ef79df
AhmedYassen <ahmed.ibrahim.yassen@gmail.com>
2023-06-14 13:17:00 +0300

    add get /dogs endpoint

--- src/api/routes/dogs.router.ts
+++ src/api/routes/dogs.router.ts
@@ -3,5 +3,6 @@ import dogController from "../controllers/DogController";
 const dogsRouter = Router();
 
 dogsRouter.post("/dogs", dogController.createDog);
+dogsRouter.get("/dogs", dogController.getDogs);
 
 export default dogsRouter;
